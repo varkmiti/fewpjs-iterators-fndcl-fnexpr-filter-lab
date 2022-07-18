fba30d1d59f6ec5dfccc43b6940b3fac5d9c7f08
Mark Viti <+>
2022-07-18 11:39:23 -0400

    commit tree b6250e9067b35d1437d9b8f16d8460b86173ad32

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// First 
+function findMatching(drivers, string) { 
+  return drivers.filter(d => d.toLowerCase() == string.toLowerCase()
+  );
+}
+
+// Second 
+function fuzzyMatch(drivers, string) { 
+  let l = string.length ;
+  return drivers.filter(d => d.slice(0, l) == string)
+}
+
+// Third 
+function matchName(drivers, string) {
+  return drivers.filter(d => d.name == string)
+}
